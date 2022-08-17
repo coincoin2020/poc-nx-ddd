3c000aa00db79bfdf8b383bcfd572da07fdec8ea
Pizzascript <emmanuel.andral@gmail.com>
2022-08-17 12:15:34 +0200

    Création de la feature 'search' (domain booking)
    Commande :
    nx g @angular-architects/ddd:feature search --domain booking --entity flight

--- apps/booking/src/app/app.module.ts
+++ apps/booking/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BookingFeatureSearchModule } from '@app/booking/feature-search';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BookingFeatureSearchModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
