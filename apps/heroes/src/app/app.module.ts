6f2aceda0d83ba833f50daa9b4e435134be1d49f
Pizzascript <emmanuel.andral@gmail.com>
2022-08-17 13:04:38 +0200

    Suppression du fichier d'exemple nx-welcome.component.ts

--- apps/heroes/src/app/app.module.ts
+++ apps/heroes/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeroesFeatureSearchModule } from '@app/heroes/feature-search';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, HeroesFeatureSearchModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
